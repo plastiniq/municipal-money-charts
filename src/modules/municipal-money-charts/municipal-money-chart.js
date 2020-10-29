55bb0604f5dcd018e17e37f2abc98ef1402ded83
Algorhythm and the Funktions <dirk.meerkotter@gmail.com>
2020-10-29 09:40:14 +0200

    Added formatRand

--- src/modules/municipal-money-charts/municipal-money-chart.js
+++ src/modules/municipal-money-charts/municipal-money-chart.js
@@ -4,6 +4,30 @@ import {format} from 'd3-format'
 
 const d3 = Object.assign({},{format})
 
+const formatRand = (x, decimals, randSpace) => {
+    decimals = decimals === undefined ? 1 : decimals; // eslint-disable-line no-param-reassign
+    randSpace = randSpace === undefined ? ' ' : ''; // eslint-disable-line no-param-reassign
+    return `R${randSpace}${d3.format(`,.${decimals}f`)(x)}`;
+};
+
+const humaniseRand = (x, longForm) => { // eslint-disable-line import/prefer-default-export
+    longForm = longForm === undefined ? true : longForm; // eslint-disable-line no-param-reassign
+    const randSpace = longForm ? ' ' : '';
+    const decimals = longForm ? 1 : 0;
+    const suffixBillion = longForm === true ? ' billion' : 'bn';
+    const suffixMillion = longForm === true ? ' million' : 'm';
+    const suffixThousand = longForm === true ? '  thousand' : 'k';
+
+    if (Math.abs(x) >= 1000000000) {
+        return formatRand(x / 1000000000, decimals, randSpace) + suffixBillion;
+    } if (Math.abs(x) >= 1000000) {
+        return formatRand(x / 1000000, decimals, randSpace) + suffixMillion;
+    } if (!longForm && Math.abs(x) >= 100000) {
+        return formatRand(x / 1000, decimals, randSpace) + suffixThousand;
+    }
+    return formatRand(x, 0);
+};
+
 let chart = {}
 
 const muniMoneyChart = (container, initialMuni, period) => {
@@ -31,7 +55,7 @@ const muniMoneyChart = (container, initialMuni, period) => {
             },
             y: {
                 tick: {
-                    format: d3.format(".2s"),
+                    format: function(d) { return humaniseRand(d, false) },
                     count: 10
                 }
             }
@@ -54,8 +78,6 @@ const muniMoneyChart = (container, initialMuni, period) => {
         }
     });
 }
-
-
 
 
 const loadData = (compareMuni, removeSeriesIds) => {
