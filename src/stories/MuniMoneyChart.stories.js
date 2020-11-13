7cd4558715a4a1ca76d715c5d5510953a2ceaca2
Algorhythm and the Funktions <dirk.meerkotter@gmail.com>
2020-11-13 09:18:53 +0200

    - Removed old redundant median code

--- src/stories/MuniMoneyChart.stories.js
+++ src/stories/MuniMoneyChart.stories.js
@@ -102,31 +102,6 @@ export const medianChart = () => {
 
     return wrapper
 
-
-// medians.push(
-//         [
-//             [paths._groups[0][0].getBBox().x, paths._groups[0][9].getBBox().x + paths._groups[0][9].getBBox().width],
-//             d3array.median([paths._groups[0][0].getBBox(), paths._groups[0][3].getBBox(), paths._groups[0][6].getBBox(), paths._groups[0][9].getBBox()], d => d.height)
-//         ],
-//         [
-//             [paths._groups[0][1].getBBox().x, paths._groups[0][10].getBBox().x + paths._groups[0][10].getBBox().width],
-//             d3array.median([paths._groups[0][1].getBBox(), paths._groups[0][4].getBBox(), paths._groups[0][7].getBBox(), paths._groups[0][10].getBBox()], d => d.height)
-//         ],
-//         [
-//             [paths._groups[0][2].getBBox().x, paths._groups[0][11].getBBox().x + paths._groups[0][11].getBBox().width],
-//             d3array.median([paths._groups[0][2].getBBox(), paths._groups[0][5].getBBox(), paths._groups[0][8].getBBox(), paths._groups[0][11].getBBox()], d => d.height)
-//         ]
-//     )
-
-//     let chartBox = d3selection.select('.c3-event-rect')
-
 } 
 
 medianChart.storyName = "Medians"
-
-
-
-//--------------------------------------------------------------------------------------//
-//                                         DATA                                         //
-//--------------------------------------------------------------------------------------//
-
